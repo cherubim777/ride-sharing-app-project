99658b204b7ebeec5e7a9f8b73970820fa705fa0
Nahom Abraham <nahom@kemercode.com>
2023-01-23 17:07:40 +0300

    Add input fields and a button for login page

--- ride_share/lib/login_page.dart
+++ ride_share/lib/login_page.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Ride Sharing App"),
+      ),
+      body: Column(
+        // mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(20.0),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'Email or phone',
+              ),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(20.0),
+            child: TextField(
+              decoration: InputDecoration(labelText: 'Password'),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Login'),
+          )
+        ],
+      ),
+    );
+    ;
   }
 }
