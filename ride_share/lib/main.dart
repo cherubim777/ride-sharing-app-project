a0cb2ddc362df0439f94ada28bdbe20a754e065f
leburik <cherub777tarsa@yahoo.com>
2023-01-24 11:55:09 +0300

    create temporary login page

--- ride_share/lib/main.dart
+++ ride_share/lib/main.dart
@@ -1,6 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:ride_share/pages/login_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:ride_share/firebase_options.dart';
+import 'package:ride_share/widget_tree.dart';
 
-void main() => runApp(
-      MaterialApp(),
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const WidgetTree(),
     );
+  }
+}
