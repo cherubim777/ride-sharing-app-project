fdaa1f7fcedad73e00d96f29cfd3909724b265ed
leburik <cherub777tarsa@yahoo.com>
2023-01-24 03:28:57 +0300

    create the WidgetTree statelful widget which control the users state

---
+++ ride_share/lib/widget_tree.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:ride_share/main.dart';
+import 'package:ride_share/auth.dart';
+import 'package:ride_share/pages/home_page.dart';
+import 'package:ride_share/pages/login_register_page.dart';
+
+class WidgetTree extends StatefulWidget {
+  const WidgetTree({super.key});
+
+  @override
+  State<WidgetTree> createState() => _WidgetTreeState();
+}
+
+class _WidgetTreeState extends State<WidgetTree> {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: Auth().authstatechanges,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return HomePage();
+        } else {
+          return const LoginPage();
+        }
+      },
+    );
+  }
+}
